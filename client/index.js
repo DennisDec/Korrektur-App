98719ed2da0c3e8972a955c9fb745148b49bc32e
Dennis <tr.ainer036@googlemail.com>
2018-03-10 22:19:10 +0100

    add iFrame and duden-search-api

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
 //document.getElementById('p1').innerHTML = "hello world"
+const DudenSearchApi = require( "duden-search-api" );
+
+let instance = new DudenSearchApi();
 
 document.getElementById('test').onsubmit = function () {
+
+  instance.search(document.getElementById('Eingabe').value).then( ( result ) => {
+     console.log( result );
+  } ).catch( ( error ) => {
+     console.log( error );
+  } );
   console.log(document.getElementById('Eingabe').value);
 	document.getElementById('p1').innerHTML = document.getElementById('Eingabe').value;
 	return false;
