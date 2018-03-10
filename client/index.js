49fe7502cda8b76b6382c87c8910f5ad0684fc2b
Dennis <tr.ainer036@googlemail.com>
2018-03-10 21:24:38 +0100

    add input field

--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
-document.getElementById('p1').innerHTML = "hello world"
+//document.getElementById('p1').innerHTML = "hello world"
+
+document.getElementById('test').onsubmit = function () {
+  console.log(document.getElementById('Eingabe').value);
+	document.getElementById('p1').innerHTML = document.getElementById('Eingabe').value;
+	return false;
+}
